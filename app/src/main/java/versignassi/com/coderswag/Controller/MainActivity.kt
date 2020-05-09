09a081b5ff717d8c88b1a14d75d75edecab46577
bruno-vargas <brunoversignassi@gmail.com>
2020-05-09 08:46:58 -0300

    Createing imageView n main activity, Creating custom layout to be used on adapter

---
+++ app/src/main/java/versignassi/com/coderswag/Controller/MainActivity.kt
@@ -0,0 +1,23 @@
+package versignassi.com.coderswag.Controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import versignassi.com.coderswag.Model.Category
+import versignassi.com.coderswag.R
+import versignassi.com.coderswag.Services.DataService
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        adapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1,
+            DataService.categories)
+        categoryListView.adapter = adapter
+    }
+}
